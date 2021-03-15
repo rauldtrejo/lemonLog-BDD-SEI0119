21983a003b91216750b6858168d3c1f50e750746
BernardaP <btp210@gmail.com>
2021-03-15 17:53:30 -0600

    adds models to the app

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Article(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.CharField(max_length=10000)
+    creation_date = models.DateField()
+    image_url = models.CharField(max_length=150)
+    rating = models.IntegerField()
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.CharField(max_length=300)
+    creation_date = models.DateField()
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+

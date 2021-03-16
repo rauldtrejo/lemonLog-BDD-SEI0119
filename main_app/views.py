1c9a952281a8a074f78882e6c8c20d36c7bf689b
rauldtrejo <rdanieltrejoguzman@gmail.com>
2021-03-16 12:41:32 -0700

    Added views for profile and profile_creation form

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,10 @@
+from main_app.forms import ProfileForm, SignupForm
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from .models import User, Profile
+from django.contrib.auth import get_user_model
 
 # Create your views here.
 
@@ -14,7 +18,7 @@ def signup(request):
   if request.method == 'POST':
     # This is how to create a 'user' form object
     # that includes the data from the browser
-    form = UserCreationForm(request.POST)
+    form = SignupForm(request.POST)
     if form.is_valid():
       # This will add the user to the database
       user = form.save()
@@ -24,8 +28,30 @@ def signup(request):
     else:
       error_message = 'Invalid sign up - try again'
   # A GET or a bad POST request, so render signup.html with an empty form
-  form = UserCreationForm()
+  form = SignupForm()
   context = {'form': form, 'error_message': error_message}
   return render(request, 'registration/signup.html', context)
+
+@login_required  
+def profile(request):
+  profile = User.objects.get(id=request.user.id)
+  print(profile)
+  return render(request, 'profile/profile.html', {'user':profile})
+
+@login_required
+def profile_creation(request):
+  # create new instance of cat form filled with submitted values or nothing
+  profile_form = ProfileForm(request.POST or None)
+  # if the form was posted and valid
+  if request.POST and profile_form.is_valid():
+    # save new instance of a cat
+    new_profile = profile_form.save(commit=False)
+    new_profile.user = request.user
+    new_profile.save()
+    # redirect to index
+    return redirect('home')
+  else:
+    # render the page with the new cat form
+    return render(request, 'profile/profileCreation.html', { 'profile_form': profile_form })
 
 

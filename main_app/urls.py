9bb2430d6d10c2dc966b36980b46334905dcf0e8
rauldtrejo <rdanieltrejoguzman@gmail.com>
2021-03-16 16:49:11 -0700

    created profile_edit url

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('accounts/signup/', views.signup, name="signup"),
     path('profile/', views.profile, name='profile'),
-    path('profile/creation/', views.profile_creation, name='profile_creation')
+    path('profile/creation/', views.profile_creation, name='profile_creation'),
+    path('profile/edit/', views.profile_edit, name='profile_edit')
 ]

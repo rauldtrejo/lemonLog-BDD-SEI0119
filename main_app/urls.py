9ec188d7cfe3e1f6774a226d1a2a722dd9a73bb5
rauldtrejo <rdanieltrejoguzman@gmail.com>
2021-03-16 12:40:27 -0700

    created paths for profile and profile creation pages

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('accounts/signup/', views.signup, name="signup"),
+    path('profile/', views.profile, name='profile'),
+    path('profile/creation/', views.profile_creation, name='profile_creation')
 ]

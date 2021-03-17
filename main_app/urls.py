7833c0e95b6302950a09d5fb050eca2a589660c3
BernardaP <btp210@gmail.com>
2021-03-17 12:28:02 -0600

    adds path to an individual review pag

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('accounts/signup/', views.signup, name="signup"),
     path('profile/', views.profile, name='profile'),
     path('profile/creation/', views.profile_creation, name='profile_creation'),
-    path('profile/edit/', views.profile_edit, name='profile_edit')
+    path('profile/edit/', views.profile_edit, name='profile_edit'),
+    path('reviews/<int:review_id>/', views.review, name='review'),
 ]
